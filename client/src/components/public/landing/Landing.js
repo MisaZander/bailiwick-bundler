cbab565ae7ca7e93eb0dcdcb4df26450b6b37bb6
Alexander Osborn <misterzander@gmail.com>
2019-01-07 20:30:46 -0600

    Get a simple navbar component up

---
+++ client/src/components/public/landing/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">O hai</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
---
+++ client/src/components/public/layout/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../img/logo-bgless.png";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/gallery">
+                Gallery
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact/form">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
