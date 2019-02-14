6fc7677e420d551b4d5728fe289db14b363cd302
Alexander Osborn <misterzander@gmail.com>
2019-02-14 12:38:20 -0600

    Strip out devtool extension

--- client/src/store.js
+++ client/src/store.js
@@ -9,10 +9,7 @@ const middleware = [thunk]; //More than one middleware can go here
 const store = createStore(
   rootReducer,
   initalState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
